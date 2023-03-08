79f82a09db57fd2cc138c746ea198f9a5522edf9
caiooliveirah09 <leelros@hotmail.com>
2023-03-08 20:23:36 -0300

    I redid the logic of a function, because I found a bug, and later I found out that it was because of the name

--- src/pages/WeatherPage.js
+++ src/pages/WeatherPage.js
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react";
 import Background from "../components/Background";
 import NavBar from "../components/NavBar";
-import getWeatherByCity from "../utils/open_weather_api";
+import getWeatherByCityUtil from "../utils/open_weather_api";
 
 function WeatherPage() {
-  const [city, setCity] = useState("São Paulo");
+  const [cityState, setCityState] = useState("");
   const [weather, setWeather] = useState(null);
+  const [defaultWeather, setDefaultWeather] = useState(null);
   const [weatherIsOk, setWeatherIsOk] = useState(false);
   const [icon, setIcon] = useState(null);
 
-  const getWeather = async () => {
-    if (city) {
-      const mainCity = await getWeatherByCity("guaratuba");
-      setWeather(mainCity);
-      setWeatherIsOk(true);
-      setIcon(
-        `https://openweathermap.org/img/wn/${mainCity.weather[0].icon}.png`
-      );
-    }
+  const getWeatherByCity = async (city) => {
+    const mainCity = await getWeatherByCityUtil(city);
+    setWeather(mainCity);
+    setWeatherIsOk(true);
+    setIcon(
+      `https://openweathermap.org/img/wn/${mainCity.weather[0].icon}.png`
+    );
+  };
+
+  const getWeatherDefault = async () => {
+    const mainCity = await getWeatherByCityUtil("São Paulo");
+    setDefaultWeather(mainCity);
+    setWeatherIsOk(true);
+    setIcon(
+      `https://openweathermap.org/img/wn/${mainCity.weather[0].icon}.png`
+    );
   };
 
   const getCityOfUser = async (position) => {
@@ -27,16 +35,15 @@ function WeatherPage() {
     const data = await result.json();
     const codeMoreCity = data.plus_code.compound_code.split(",")[0];
     const city = codeMoreCity.split(" ")[1];
-    setCity(city);
+    if (city) getWeatherByCity(city);
+    setCityState(city);
   };
 
   useEffect(() => {
+    getWeatherDefault();
     navigator.geolocation.getCurrentPosition(getCityOfUser);
   }, []);
 
-  useEffect(() => {
-    getWeather();
-  }, [city]);
   return (
     <div>
       <div>
@@ -46,13 +53,13 @@ function WeatherPage() {
       <div className="w-screen h-screen flex justify-center items-center flex-col">
         <div className="flex justify-center items-center flex-col filter-none text-gray-100 z-10 h-1/4">
           <h1 className="text-5xl font-normal tracking-widest max-md:text-2xl uppercase">
-            {city}
+            {cityState ? cityState : "São Paulo"}
           </h1>
         </div>
         <div className="bg-white w-full flex flex-col drop-shadow-2xl h-full justify-center max-h-screen max-w-3xl">
           <div className="flex justify-center mx-3">
             <div className="w-full h-full max-w-md flex h-80">
-              {weather && weatherIsOk && (
+              {(weather || defaultWeather) && weatherIsOk && (
                 <div className="uppercase my-8 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-gray-50 w-full drop-shadow-2xl h-full flex flex-col justify-center items-center">
                   <div className="flex">
                     <svg
@@ -74,18 +81,33 @@ function WeatherPage() {
                         d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
                       />
                     </svg>
-                    <h2 className="text-2xl pt-2 ">{weather.name}</h2>
+                    <h2 className="text-2xl pt-2 ">
+                      {!weather ? defaultWeather.name : weather.name}
+                    </h2>
                   </div>
                   <h3 className="text-5xl my-4">
-                    {parseInt(weather.main.temp)} °C
+                    {parseInt(
+                      !weather ? defaultWeather.main.temp : weather.main.temp
+                    )}{" "}
+                    °C
                   </h3>
                   {icon && <img src={icon}></img>}
                   <div>
-                    <span>{weather.weather[0].description}</span>
+                    <span>
+                      {!weather
+                        ? defaultWeather.weather[0].description
+                        : weather.weather[0].description}
+                    </span>
                   </div>
                   <div>
                     <span>
-                      sensação térmica de {parseInt(weather.main.feels_like)} °C
+                      sensação térmica de{" "}
+                      {parseInt(
+                        !weather
+                          ? defaultWeather.main.feels_like
+                          : weather.main.feels_like
+                      )}{" "}
+                      °C
                     </span>
                   </div>
                 </div>
