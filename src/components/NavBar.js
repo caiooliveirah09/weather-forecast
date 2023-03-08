527193f4fe79b959f71e570741d2a8e91d0e1281
caiooliveirah09 <leelros@hotmail.com>
2023-03-08 18:17:00 -0300

    add margin in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="w-full h-10 fixed z-20 text-gray-100 mb-4 uppercase font-semibold text-sm">
+    <div className="w-full h-10 fixed z-20 text-gray-100 mb-4 uppercase font-semibold text-sm mt-3 ml-3">
       <nav className="">
         <Link className="mr-5" to="/">
           página inicial
