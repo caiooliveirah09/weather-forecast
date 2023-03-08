db083c7aca4ad8748827027c11b3023d51c83536
caiooliveirah09 <leelros@hotmail.com>
2023-03-08 15:51:00 -0300

    created components, background and navbar

---
+++ src/components/Background.js
@@ -0,0 +1,7 @@
+function Background() {
+  return (
+    <div className="bg-main w-full h-2/5 bg-cover drop-shadow-2xl flex justify-center items-center blur-sm top-0 absolute z-0" />
+  );
+}
+
+export default Background;
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div className="w-full h-10 fixed z-20 text-gray-100 mb-4 uppercase font-semibold text-sm">
+      <nav className="">
+        <Link className="mr-5" to="/">
+          página inicial
+        </Link>
+        <Link className="mr-5" to="/search">
+          buscar cidade
+        </Link>
+        <Link to="/contact">contato</Link>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
