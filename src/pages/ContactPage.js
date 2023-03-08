a239a94f472ebe4c5cc8d1d49b72f46638127c35
caiooliveirah09 <leelros@hotmail.com>
2023-03-08 15:50:46 -0300

    created a contactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import Background from "../components/Background";
+import NavBar from "../components/NavBar";
+
+const submitButtonDisabled =
+  "bg-gray-400 w-4/12 flex justify-center items-center uppercase my-2 h-10 text-gray-100 text-sm";
+const submitButtonNotDisabled =
+  "bg-gradient-to-r from-violet-500 to-fuchsia-500 w-4/12 flex justify-center items-center uppercase my-2 h-10 text-gray-100 text-sm";
 
 function ContactPage() {
   const [name, setName] = useState("");
@@ -6,6 +13,11 @@ function ContactPage() {
   const [subject, setSubject] = useState("");
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState("");
+  const [submitIsDisabled, setSubmitIsDisabled] = useState(true);
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [subjectError, setSubjectError] = useState("");
+  const [messageError, setMessageError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,45 +30,129 @@ function ContactPage() {
     });
   };
 
+  const handleName = (event) => {
+    setName(event.target.value);
+    event.target.value
+      ? setNameError("")
+      : setNameError("Por favor, digite seu nome");
+  };
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    if (!event.target.value) {
+      setEmailError("Por favor, digite seu email");
+    } else if (!/\S+@\S+\.\S+/.test(event.target.value)) {
+      setEmailError("Por favor, digite um email válido");
+    } else {
+      setEmailError("");
+    }
+  };
+  const handleSubject = (event) => {
+    setSubject(event.target.value);
+    event.target.value
+      ? setSubjectError("")
+      : setSubjectError("Por favor, digite o assunto");
+  };
+  const handleMessage = (event) => {
+    setMessage(event.target.value);
+    event.target.value
+      ? setMessageError("")
+      : setMessageError("Por favor, digite a descrição de sua mensagem");
+  };
+
   return (
-    <div className="contact-page">
-      <h1>Contato</h1>
-      <h2>Fale com a gente</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          placeholder="seu nome"
-        />
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          placeholder="seu e-mail"
-        />
-        <input
-          type="text"
-          id="subject"
-          value={subject}
-          onChange={(event) => setSubject(event.target.value)}
-          placeholder="assunto"
-        />
-        <input
-          type="file"
-          id="file"
-          onChange={(event) => setFile(event.target.files[0])}
-        />
-        <textarea
-          id="message"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          placeholder="descrição"
-        />
-        <button type="submit">enviar</button>
-      </form>
+    <div>
+      <div>
+        <Background />
+        <NavBar />
+      </div>
+      <div className="w-screen h-screen flex justify-center items-center flex-col">
+        <div className="flex justify-center items-center flex-col filter-none text-gray-100 z-10 h-1/4">
+          <h1 className="text-5xl font-normal tracking-widest max-md:text-2xl uppercase">
+            Contate-nos
+          </h1>
+          <h2 className="text-2xl font-light max-md:text-xl">
+            Fale com a gente
+          </h2>
+        </div>
+        <div className="bg-white w-full flex flex-col drop-shadow-2xl h-full justify-center max-h-screen max-w-3xl">
+          <div className="flex justify-center mx-3">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col mt-4 justify-center items-center w-full h-full max-w-xl"
+            >
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={handleName}
+                placeholder="seu nome"
+                className=" w-full max-w-screen-sm text-sm border-gray-400 border h-9 my-3 px-2 h-14 max-md:my-1"
+              />
+              {nameError && <div className="text-sm">{nameError}</div>}
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmail}
+                placeholder="seu e-mail"
+                className=" w-full max-w-screen-sm text-sm border-gray-400 border h-9 my-3 px-2 h-14 max-md:my-1"
+              />
+              {emailError && <div className="text-sm">{emailError}</div>}
+              <input
+                type="text"
+                id="subject"
+                value={subject}
+                onChange={handleSubject}
+                placeholder="assunto"
+                className=" w-full max-w-screen-sm text-sm border-gray-400 border h-9 my-3 px-2 h-14 max-md:my-1"
+              />
+              {subjectError && <div className="text-sm">{subjectError}</div>}
+              <label
+                htmlFor="file"
+                className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-4/12 flex justify-center items-center uppercase my-2 h-10 text-gray-50 text-sm max-md:my-1"
+              >
+                Escolher ficheiro
+              </label>
+              <input
+                type="file"
+                id="file"
+                onChange={(event) => setFile(event.target.files[0])}
+                className="hidden"
+              />
+              {file && (
+                <div className="text-sm">
+                  arquivo escolhido com sucesso! <br></br> {file.name}{" "}
+                  <button
+                    className=" text-red-500"
+                    onClick={(event) => setFile(null)}
+                  >
+                    X
+                  </button>
+                </div>
+              )}
+              <textarea
+                id="message"
+                value={message}
+                onChange={handleMessage}
+                placeholder="descrição"
+                className="text-sm border border-gray-400 w-full my-3 p-2 max-h-40 h-1/3 max-md:my-1"
+              />
+              {messageError && <div className="text-sm">{messageError}</div>}
+              <button
+                className={
+                  submitIsDisabled
+                    ? submitButtonDisabled
+                    : submitButtonNotDisabled
+                }
+                type="submit"
+                disabled={submitIsDisabled}
+              >
+                enviar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main: "url('/public/eb902ba0-0799-45d3-87c0-aab3f6af5cc2.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
