4085a8d8b72d1a361f6d0de995e7a5742c9f5fb0
Elros <97924292+caiooliveirah09@users.noreply.github.com>
2023-03-11 22:49:22 -0300

    Update WeatherPage.js

--- src/pages/WeatherPage.js
+++ src/pages/WeatherPage.js
@@ -30,7 +30,7 @@ function WeatherPage() {
 
   const getCityOfUser = async (position) => {
     const result = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&result_type=locality&key=AIzaSyCxAgEoVCW_DkmqoSAEPjsfyZHNEB2dVrs`
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&result_type=locality&key={Process.env.API_GOOGLE}`
     );
     const data = await result.json();
     const codeMoreCity = data.plus_code.compound_code.split(",")[0];
