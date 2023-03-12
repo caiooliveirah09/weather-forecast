0daa498fcc9f2465951786c60441d5fc10d5ddc0
Elros <97924292+caiooliveirah09@users.noreply.github.com>
2023-03-11 22:50:04 -0300

    Update open_weather_api.js

--- src/utils/open_weather_api.js
+++ src/utils/open_weather_api.js
@@ -1,6 +1,6 @@
 const getWeatherByCity = async (city) => {
   const cities = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=dfc296c2e37a7ab5a4ba509cd53d8685&lang=pt_br`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={Process.env.OPEN_WEATHER_APPID}&lang=pt_br`
   );
   const data = await cities.json();
   return data;
